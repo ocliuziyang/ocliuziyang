c3e9e81fcd5e774b73fe2da38ac0d66682cf53c2
ocliuziyang <ocliuziyang@gmail.com>
2017-05-05 17:16:52 +0800

    init project, change welcome page

---
+++ config/website.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'ocliuziyang',
+    'link' => [
+        'blog' => 'http://ocliuziyang.dev',
+        'github' => 'http://github.com/ocliuziyang',
+        'weibo' => 'http://weibo.com/p/1005053844666952/home?from=page_100505&mod=TAB&is_all=1#place'
+    ]
+];
